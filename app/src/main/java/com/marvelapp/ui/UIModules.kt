77681474e743a71d3d4654ad2683f0421237d7db
haby <haby.romaric@gmail.com>
2022-05-20 12:06:12 +0200

    fix bug view model

--- app/src/main/java/com/marvelapp/ui/UIModules.kt
+++ app/src/main/java/com/marvelapp/ui/UIModules.kt
@@ -1,5 +1,6 @@
 package com.marvelapp.ui
 
+import com.marvelapp.ui.fragment.characterdetail.DetailCharacterViewModel
 import com.marvelapp.ui.fragment.characterlist.CharacterListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,6 +9,9 @@ object UIModules {
     private val uiModules = module {
         viewModel {
             CharacterListViewModel(get())
+        }
+        viewModel {
+            DetailCharacterViewModel(get(), get())
         }
     }
 
